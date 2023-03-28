bb57ea5f274aaca040b68d89f5718e5f316d2baf
xpfoxs <46740443+xpfoxs@users.noreply.github.com>
2023-03-29 04:57:44 +0800

    Add files via upload

---
+++ user/immortalwrt18.06/custom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "开始运行User/custom.sh脚本"
+
+cp -f ../user/common/default-settings package/emortal/default-settings/files/99-default-settings
+
+cat ../user/immortalwrt18.06/config.diff ../user/common/config.diff > .config
+
+echo "结束运行User/custom.sh脚本"
