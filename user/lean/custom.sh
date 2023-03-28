8300cb0a8564b3cc9175c23154110a4c00cef0d4
xpfoxs <46740443+xpfoxs@users.noreply.github.com>
2023-03-28 23:38:36 +0800

    Update custom.sh

--- user/lean/custom.sh
+++ user/lean/custom.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo "运行custom.sh脚本"
-
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/packages/libs/libcap
-svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap             feeds/packages/libs/libcap
-git clone https://github.com/destan19/OpenAppFilter.git                         package/OpenAppFilter
-rm -rf feeds/packages/net/mosdns
-git clone https://github.com/KFERMercer/luci-app-tcpdump.git                    package/luci-app-tcpdump
+echo "User/运行custom.sh脚本"
 
 cp -f ../user/common/default-settings package/lean/default-settings/files/zzz-default-settings
