df4ae28969b2bde1f4a2be6981efc57d71092a8d
xpfoxs <46740443+xpfoxs@users.noreply.github.com>
2023-03-29 23:03:57 +0800

    Delete user/lienol directory

--- user/lienol/custom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "开始运行User/custom.sh脚本"
-
-cp -f ../user/common/default-settings package/default-settings/files/zzz-default-settings
-
-#cat ../user/lienol/config.diff ../user/common/config.diff > .config
-cat ../user/lienol/config.diff > .config
-echo "结束运行User/custom.sh脚本"
