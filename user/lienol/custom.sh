5766c133c1187001c1c9c96b9212fe0280cc4cc6
xpfoxs <46740443+xpfoxs@users.noreply.github.com>
2023-03-29 19:13:29 +0800

    Update custom.sh

--- user/lienol/custom.sh
+++ user/lienol/custom.sh
@@ -4,6 +4,6 @@ echo "开始运行User/custom.sh脚本"
 
 cp -f ../user/common/default-settings package/default-settings/files/zzz-default-settings
 
-cat ../user/lienol/config.diff ../user/common/config.diff > .config
+#cat ../user/lienol/config.diff ../user/common/config.diff > .config
 
 echo "结束运行User/custom.sh脚本"
