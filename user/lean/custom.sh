a166e30d18d5624433cf2f78cabf3f4a24588e5c
xpfoxs <46740443+xpfoxs@users.noreply.github.com>
2023-03-29 19:48:47 +0800

    Update custom.sh

--- user/lean/custom.sh
+++ user/lean/custom.sh
@@ -5,5 +5,5 @@ echo "开始运行User/custom.sh脚本"
 cp -f ../user/common/default-settings package/lean/default-settings/files/zzz-default-settings
 
 #cat ../user/lean/config.diff ../user/common/config.diff > .config
-
+cat ../user/lean/config.diff > .config
 echo "结束运行User/custom.sh脚本"
