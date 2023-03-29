f60402ac7ef9993438910e39aaba4b2e3e056ed3
xpfoxs <46740443+xpfoxs@users.noreply.github.com>
2023-03-29 19:12:40 +0800

    Update custom.sh

--- user/immortalwrt18.06/custom.sh
+++ user/immortalwrt18.06/custom.sh
@@ -4,6 +4,6 @@ echo "开始运行User/custom.sh脚本"
 
 cp -f ../user/common/default-settings package/emortal/default-settings/files/99-default-settings
 
-cat ../user/immortalwrt18.06/config.diff ../user/common/config.diff > .config
+#cat ../user/immortalwrt18.06/config.diff ../user/common/config.diff > .config
 
 echo "结束运行User/custom.sh脚本"
