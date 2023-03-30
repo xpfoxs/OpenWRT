c0771d19113cbdfc48a6c1fef19d58064686322f
XpFox <46740443+xpfoxs@users.noreply.github.com>
2023-03-30 19:10:55 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 echo "开始运行Common/custom.sh脚本"
+
+#2. Custom settings
+#sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
+#sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
+#sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
+
 echo "结束运行Common/custom.sh脚本"
