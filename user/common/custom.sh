0e7779ee11ca3e37a9ccb01e729d535b0fdbd317
XpFox <46740443+xpfoxs@users.noreply.github.com>
2023-03-31 04:27:56 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -8,7 +8,7 @@ echo "开始运行Common/custom.sh脚本"
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 #2. 更换golang版本
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 
 echo "结束运行Common/custom.sh脚本"
